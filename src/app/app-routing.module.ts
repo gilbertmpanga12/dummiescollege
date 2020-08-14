711991b79b617f927d7e34c67daba4262e37e4e8
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2020-08-14 13:26:34 +0300

    Update;- completed mainNavbar  view setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
-const routes: Routes = [{ path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }, { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }, { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) }];
+const routes: Routes = [
+  {path: '',component: LayoutComponent,children: [
+    { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule)},
+    { path: 'skills-and-work', loadChildren:
+     () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) },
+     { path: 'notifications', loadChildren: 
+    () => import('./notifications/notifications.module').then(m => m.NotificationsModule) }
+  ]}, 
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
