7fc2487c500ded66b08a695d200091cdc81afdd4
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2020-08-19 12:35:38 +0300

    Update;- generated service

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -2,8 +2,25 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AuthComponent } from './auth.component';
+import {LoginComponent} from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { ForgotpasswordComponent } from './forgotpassword/forgotpassword.component';
 
-const routes: Routes = [{ path: '', component: AuthComponent }];
+
+const routes: Routes = [{ path: '', component: AuthComponent , children: [
+  {
+    path: '',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegistrationComponent
+  },
+  {
+    path: 'forgot-password',
+    component: ForgotpasswordComponent
+  }
+]}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
+import { ForgotpasswordComponent } from './forgotpassword/forgotpassword.component';
 
 
 @NgModule({
-  declarations: [AuthComponent],
+  declarations: [AuthComponent, RegistrationComponent, LoginComponent, ForgotpasswordComponent],
   imports: [
     CommonModule,
     AuthRoutingModule
