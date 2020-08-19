730e5c6c27e29eaffcdcba0cca4cbfcfa737726c
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2020-08-19 15:49:29 +0300

    Update;- created guard and minor changes :

--- src/app/services/main.service.ts
+++ src/app/services/main.service.ts
@@ -1,14 +1,52 @@
 import { Injectable } from '@angular/core';
+import { User } from 'firebase';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { HttpClient } from '@angular/common/http';
+import { firestore as ft } from 'firebase/app';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MainService {
+  user: User;
   year: Date = new Date();
+  userVerified: boolean = false;
   fullYear: number = this.year.getFullYear();
   isLoading: boolean = false;
-  constructor() { 
-   
+  constructor(private auth: AngularFireAuth, 
+    private router: Router, private firestore: AngularFirestore,
+     private http: HttpClient) { 
+      this.auth.authState.subscribe(user => {
+        if (user){
+          this.user = user;
+          this.userVerified = user.emailVerified;
+        }
+      });
   }
+
+  async login(email: string, password: string){
+    await this.auth.signInWithEmailAndPassword(email,password);
+    this.router.navigate(['/']);
+  }
+
+  async registerAccount(email: string, password: string, accountType: string){
+    await this.auth.createUserWithEmailAndPassword(email,password);
+    this.router.navigate(['/']);
+  }
+
+  async forgotPassword(email: string){
+    return await this.auth.sendPasswordResetEmail(email);
+  }
+
+  async sendEmailVerification() {
+    await (await this.auth.currentUser).sendEmailVerification();
+    this.router.navigate(['/']);
+  }
+
+
+  
 
 }
