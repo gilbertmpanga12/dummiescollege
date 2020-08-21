60aa93b178d43e6159da494f3397a89368eb07b7
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2020-08-21 11:22:58 +0300

    Update;- ui for confirmation email added

---
+++ src/app/emailactivation/emailactivation.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EmailactivationComponent } from './emailactivation/emailactivation.component';
+
+
+
+@NgModule({
+  declarations: [EmailactivationComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class EmailactivationModule { }
