037dae662f719291af5d74dae1232f473c21444c
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2020-08-22 21:09:01 +0300

    Update;- minor changes

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-
+import { DragScrollModule } from 'ngx-drag-scroll';
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    DragScrollModule
   ]
 })
 export class HomeModule { }
