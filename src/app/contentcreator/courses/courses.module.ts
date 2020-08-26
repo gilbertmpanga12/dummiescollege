7e5baceab14d1554bcf95a19ebbb69e0adc22b01
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2020-08-26 14:30:21 +0300

    Update; course list completed

--- src/app/contentcreator/courses/courses.module.ts
+++ src/app/contentcreator/courses/courses.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses.component';
+import { NavModule } from 'src/app/sharables/nav/nav.module';
+import { BottomnavModule } from 'src/app/sharables/bottomnav/bottomnav.module';
 
 
 @NgModule({
   declarations: [CoursesComponent],
   imports: [
     CommonModule,
-    CoursesRoutingModule
+    CoursesRoutingModule,
+    NavModule,
+    BottomnavModule
   ]
 })
 export class CoursesModule { }
