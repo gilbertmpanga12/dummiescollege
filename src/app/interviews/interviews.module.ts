d94218a019d697fcab4ab7a1f7bd36d4d406a16e
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2020-08-28 22:16:40 +0300

    Update;- interview questions completed

--- src/app/interviews/interviews.module.ts
+++ src/app/interviews/interviews.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { InterviewsRoutingModule } from './interviews-routing.module';
 import { InterviewsComponent } from './interviews.component';
+import { NavModule } from '../sharables/nav/nav.module';
+import { BottomnavModule } from '../sharables/bottomnav/bottomnav.module';
 
 
 @NgModule({
   declarations: [InterviewsComponent],
   imports: [
     CommonModule,
-    InterviewsRoutingModule
+    InterviewsRoutingModule,
+    NavModule,
+    BottomnavModule
   ]
 })
 export class InterviewsModule { }
