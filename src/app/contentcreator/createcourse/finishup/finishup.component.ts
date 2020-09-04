ca922bd6dcb1a4890bb2e6fc6e75b245cf14c59e
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2020-09-04 20:14:04 +0300

    Update;- added custom message wwhen fully completed course

--- src/app/contentcreator/createcourse/finishup/finishup.component.ts
+++ src/app/contentcreator/createcourse/finishup/finishup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MainService } from 'src/app/services/main.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-finishup',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FinishupComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: MainService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  finishUp(): void{
+    this.router.navigate(['/dashboard']);
+    this.service.toast('Your course was published successfully', 'success');
+    this.service.clearImportantCredentials();
   }
 
 }
