fdb714261599035cb1618b906caf84bf6defdd39
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2020-09-06 17:47:52 +0300

    Update;- completed copy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,14 +7,14 @@ export const environment = {
   algolia_app_id: 'FG4VUJQIMN',
   algolia_api_key: 'fea3bb91c6d71f5f22600ac0ba063b42',
   firebase: {
-    apiKey: "AIzaSyD4pR0VWYdnLP4VUW_mXBKIleFK2mWFqvc",
-    authDomain: "dummiescollege.firebaseapp.com",
-    databaseURL: "https://dummiescollege.firebaseio.com",
-    projectId: "dummiescollege",
-    storageBucket: "dummiescollege.appspot.com",
-    messagingSenderId: "692824260415",
-    appId: "1:692824260415:web:7834a713ba58bdbdb91d8a",
-    measurementId: "G-V1W9VNP8H8"
+    apiKey: "AIzaSyAIlycYv6eFiWMOxDMpy4h_jbHT4YjH2h4",
+    authDomain: "dummiescollege-9106f.firebaseapp.com",
+    databaseURL: "https://dummiescollege-9106f.firebaseio.com",
+    projectId: "dummiescollege-9106f",
+    storageBucket: "dummiescollege-9106f.appspot.com",
+    messagingSenderId: "336610836872",
+    appId: "1:336610836872:web:6ed3e19e87d0e495a27a3d",
+    measurementId: "G-CHZMLCMFMD"
   },
  baseUrl: 'https://dummiescollege.herokuapp.com/'
 };
